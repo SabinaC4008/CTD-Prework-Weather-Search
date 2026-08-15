9a08179af17df7e6cf2ccbb85f0e546af08607b1
Sabina <sabinachauhan4008@gmail.com>
2026-08-15 16:49:06 -0700

    Added functionality to search button to fetch location information such as latitude, longitude, and timezone. These values then are used to fetch the temperature, weather condition, and uv index

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,42 @@ import './App.css'
 
 function App() {
   const [formState, setFormState] = useState('Temperature') 
-
+  const [city, setCity] = useState('')
   const uvHandler = () => setFormState('UV')
   const tempHandler = () => setFormState('Temperature')
   const weatherHandler = () => setFormState('Weather Conditions')
-  const formHandler = (event) => {
+  const cityHandler = (event) => setCity(event.target.value)
+  
+  const searchHandler = (event) => {
     event.preventDefault()
+    getCity()
   }
 
 
   const getCity = async () => {
-    const locationResponse = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=Berlin&count=1&language=en&format=json`)
+    const locationResponse = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=en&format=json`)
     const possibleLocations = await locationResponse.json()
     const location = possibleLocations.results[0]
     console.log(location)
+    const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${location.latitude}&longitude=${location.longitude}&daily=uv_index_max&timezone=${location.timezone}&forecast_days=1`)
+    const weather = await weatherResponse.json()
+    console.log(weather)
   }
-  getCity()
 
   return (
     <>
       <div>
-        <form>
-          <input type="text" onSubmit={formHandler}/> 
+        <form onSubmit={searchHandler}>
+          <input type="text" onChange={cityHandler}/> 
           <button type="submit">Search</button>
         </form>
         <button onClick={tempHandler}>Temperature 🌡️</button>
         <button onClick={weatherHandler}>Weather Condition 🌥️</button>
         <button onClick={uvHandler}>UV Index ☀️</button>
         <div>
-          {formState == 'UV' ? 'yes' : 'no'}
+          {formState == 'UV' ? 'yes' : 'no'} 
+          {//Testing to see if states change based on button clicks above
+          }
         </div>
       </div>
     </>
