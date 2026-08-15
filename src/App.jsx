d5ece901339e4aea25d49571423633edd41faccc
Sabina <sabinachauhan4008@gmail.com>
2026-08-15 14:23:24 -0700

    Created a async function to fetch city information. Fetch request works and promise response is printed onto console.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import './App.css'
 
 function App() {
+  const getCity = async () => {
+    const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=Berlin&count=10&language=en&format=json`)
+    console.log(response.json())
+  }
+  getCity()
+
   return (
     <>
       <div>
