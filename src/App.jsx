be9335014a283264dd0a789ccac2b1633ee8ca8f
Sabina <sabinachauhan4008@gmail.com>
2026-08-16 08:51:21 -0700

    Created different functions to handle each of the different data end-points when the different buttons are pressed. Changed visuals to center content and text.

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,98 @@
 import { useState} from 'react'
-import './App.css'
 
 function App() {
   const [formState, setFormState] = useState('Temperature') 
   const [city, setCity] = useState('')
-
+  const [locationInfo, setlocationInfo] = useState('')
   const [info, setInfo] = useState('')
 
-  const uvHandler = () => setFormState('UV')
-  const tempHandler = () => setFormState('Temperature')
-  const weatherHandler = () => setFormState('Weather Conditions')
+
+  //Button Handlers 
+  const uvHandler = () => {
+    setFormState('UV')
+    if (locationInfo){
+      getUV(locationInfo)
+    }
+  }
+
+  const tempHandler = () => {
+    setFormState('Temperature')
+    if (locationInfo){
+      getTemperature(locationInfo)
+    }
+  }
+
+  const weatherHandler = () => {
+    setFormState('Weather Conditions')
+    if (locationInfo){
+      getWeather(locationInfo)  
+    }
+  }
+
   const cityHandler = (event) => setCity(event.target.value)
   
   const searchHandler = async (event) => {
     event.preventDefault()
-    getCityWeather()
+    getWeatherState(formState)
   }
 
-  const getCityWeather = async () => {
+  const getWeatherState = async (infoToGet) => {
     const locationResponse = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=en&format=json`)
     const possibleLocations = await locationResponse.json()
     const location = possibleLocations.results[0]
     console.log(location)
+    setlocationInfo(location)
 
-    if(formState == 'Temperature') {
-      const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${location.latitude}&longitude=${location.longitude}&current=temperature_2m&timezone=${location.timezone}&forecast_days=1`)
-      const weather = await weatherResponse.json()
-      console.log('Temperature')
-      console.log(weather.current.temperature_2m) //Need to still change this to temperature 
-      setInfo("Temperature:" + weather.current.temperature_2m)
+    //Selecting which info to return right now
+    if(infoToGet == 'Temperature') {
+      getTemperature(location)
     }
-
-    if(formState == 'UV') {
-      const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${location.latitude}&longitude=${location.longitude}&daily=uv_index_max&timezone=${location.timezone}&forecast_days=1`)
-      const weather = await weatherResponse.json()
-      console.log('UV')
-      console.log(weather.daily.uv_index_max[0]) //Need to still change this to temperature 
-      setInfo("UV:" + weather.daily.uv_index_max[0])
+    if(infoToGet == 'UV') {
+      getUV(location)
     }
-
-    if(formState == 'Weather Conditions') {
-      const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${location.latitude}&longitude=${location.longitude}&current=weather_code&timezone=${location.timezone}&forecast_days=1`)
-      const weather = await weatherResponse.json()
-      console.log('Weather Conditions')
-      console.log(weather.current.weather_code) //Need to still change this to Weather Conditions 
-      setInfo("Weather Conditions:" + weather.current.weather_code)
+    if(infoToGet == 'Weather Conditions') {
+      getWeather(location)
     }
 
   }
 
+
+  //Functions for getting individual info
+  //Function for fetching Temperature
+  const getTemperature = async (location) => {
+    console.log(location)
+    const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${location.latitude}&longitude=${location.longitude}&current=temperature_2m&timezone=${location.timezone}&forecast_days=1`)
+    const weather = await weatherResponse.json()
+    //console.log('Temperature')
+    //console.log(weather.current.temperature_2m)
+    setInfo("Current temperature is: " + weather.current.temperature_2m)
+  }
+
+  //Function for fetching UV
+  const getUV = async (location) => {
+    const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${location.latitude}&longitude=${location.longitude}&daily=uv_index_max&timezone=${location.timezone}&forecast_days=1`)
+    const weather = await weatherResponse.json()
+    //console.log('UV')
+    //console.log(weather.daily.uv_index_max[0])
+    setInfo("Current UV levels are: " + weather.daily.uv_index_max[0])
+  }
+
+  //Function for fetching Weather Code 
+  const getWeather = async (location) => {
+    const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${location.latitude}&longitude=${location.longitude}&current=weather_code&timezone=${location.timezone}&forecast_days=1`)
+    const weather = await weatherResponse.json()
+    //console.log('Weather Conditions')
+    //console.log(weather.current.weather_code)
+    setInfo(" Current Weather Conditions are: " + weather.current.weather_code)
+  }
+
   return (
     <>
-      <div>
-        {formState}
+      <div className='main-container'>
+        Weather Search Platform
+        <div>
+          {formState}
+        </div>
         <form onSubmit={searchHandler}>
           <input type="text" onChange={cityHandler}/> 
           <button type="submit">Search</button>
